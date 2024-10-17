04ad9c82fb08a86804c4d1e02b98313f69d3589f
mh105 <alexhe0218@gmail.com>
2024-10-17 13:48:13 -0700

    BF: fix a bug with yaxis access

--- plot_utils.py
+++ plot_utils.py
@@ -16,7 +16,7 @@ def plot_resmat_metrics(res_mat, src, legend_str):
     fig, ax = plt.subplots(1, 2, figsize=(15, 5))
     ax[0].hist(SD, bins=bins, alpha=0.5, density=True, edgecolor='black', label=legend_str)
     ax[0].legend(prop={'size': 10})
-    ax[0].gca().yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax[0].yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     ax[0].xlabel('SD (cm)', fontsize=font_size)
     ax[0].ylabel('pmf', fontsize=font_size)
     ax[0].xticks(fontsize=font_size)
@@ -25,7 +25,7 @@ def plot_resmat_metrics(res_mat, src, legend_str):
 
     ax[1].hist(DLE, bins=bins, alpha=0.5, density=True, edgecolor='black',  label=legend_str)
     ax[1].legend(prop={'size': 10})
-    ax[1].gca().yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax[1].yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     ax[1].xlabel('DLE (cm)', fontsize=font_size)
     ax[1].ylabel('pmf', fontsize=font_size)
     ax[1].xticks(fontsize=font_size)
